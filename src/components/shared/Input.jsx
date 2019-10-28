22feee3a92f1e21b3188de2732df8f3b4d9edc91
Olga Bolotova <olbolot@gmail.com>
2019-10-28 18:59:22 +0300

    Add input and submit components

---
+++ src/components/shared/Input.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from 'styled-components';
+
+const StyledInput = styles.input`
+    margin-bottom: 40px;
+    width: 100%;
+    padding: 8px 0;
+    font-size: 16px;
+    border: 0;
+    border-bottom: 2px solid #eaeaea;
+    transition: border-color 0.4s;
+    
+    &:hover {
+        border-color: #1473e6;
+    }
+          
+    &:focus {
+        outline: none;
+        border-color: #1473e6;
+    }
+`;
+
+const Input = ({ type, id, placeholder }) => {
+    return (
+        <>
+            <StyledInput type={type} id={id} placeholder={placeholder} />
+        </>
+    );
+};
+
+export default Input;
