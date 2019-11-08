b4b1fc0fc82a881909c7b9087993a8ab396eb5d1
Olga Bolotova <olbolot@gmail.com>
2019-11-08 15:50:48 +0300

    Add code splitting

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import Layout from '../Layout';
+import React, { Suspense } from 'react';
 import AuthPage from '../AuthPage';
 import { AuthContext } from '../AuthContext';
+
+const Layout = React.lazy(() => import('../Layout'));
 
 const Main: React.FC = () => {
     const { isAuth } = React.useContext(AuthContext);
 
-    return isAuth ? <Layout /> : <AuthPage />;
+    return isAuth ? (
+        <Suspense fallback="Загрузка">
+            <Layout />
+        </Suspense>
+    ) : (
+        <AuthPage />
+    );
 };
 
 export default Main;
